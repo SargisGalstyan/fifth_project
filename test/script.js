5bd1a0323f0c63fdae96834d29e067d27d73d687
SargisGalstyan <94855634+SargisGalstyan@users.noreply.github.com>
2021-12-04 14:58:33 +0400

    test

---
+++ test/script.js
@@ -0,0 +1,14 @@
+let age = prompt("how old are you");
+if(age < 18) {
+alert("hm, come back with dad");
+}
+if(age > 18 && age < 21) {
+alert("nearly but not yet bro");
+}
+if(age >= 21) {
+alert("welcome to the club")
+}
+
+// let a = 0;
+// a += prompt("enter the number");
+// alert(a);
